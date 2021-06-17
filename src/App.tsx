452381bf55add0f27975853e830ce5b64f1cfbe5
frannie <ziesemer@mc-quadrat.com>
2021-06-17 19:01:16 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  //create state to hold onto data we want to display to user
+  const [cardsFound, setCardsFound] = useState([]);
+  const [cardSearch, setCardSearch] = useState('');
+
+  // api call const searchForRecipes async await
+  
+  const searchForMagicCards = async (query: string): Promise<any> => {
+    const result = await fetch(`https://api.magicthegathering.io/v1/cards?name=${query}`);
+    return (await result.json()).cards;
+  }
+
+  const search = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const input = form.querySelector('#searchText') as HTMLInputElement;  //demonstrating how to set type to query selectors etc. 
+    setCardSearch(input.value);
+  }
+
+  useEffect(() => {
+    (async () => {
+      const query = encodeURIComponent(cardSearch);
+      if(query) {
+        console.log(query)
+        const response = await searchForMagicCards(query);
+        console.log(response)
+        setCardsFound(response);
+      }
+      })()
+  }, [cardSearch]); //this is a dependency - only calls the useEffect if card search
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Card Search App</h1>
+      <form className="searchForm" onSubmit={event => search(event)}>
+        <input id="searchText" type="text" />
+        <button>Search</button>
+      </form>
+      
     </div>
   );
 }
